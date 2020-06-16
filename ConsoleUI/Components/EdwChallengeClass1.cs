32ecfe09d27dd4d84151dacd6ac7ab1673999e79
CodeweaverNG <jereco11@gmail.com>
2020-06-16 19:23:58 +0400

    Implemented EDW_Challenge

--- ConsoleUI/Components/EdwChallengeClass1.cs
+++ ConsoleUI/Components/EdwChallengeClass1.cs
@@ -1,18 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ComponentLoader;
 using ComponentLoader.Abstract;
 
+[assembly: EDW_Challenge("EDW CHallenge")]
 namespace ComponentLoader.Components
 {
+
+
     /// <summary>
-    /// 
+    /// EdwChallengeClass1 SamPle Class Implementing the IEdwChallenge Interface.
     /// </summary>
+    
     public class EdwChallengeClass1 : IEdwChallenge
     {
         public void Report()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// EdwChallengeClass2 SamPle Class Implementing the IEdwChallenge Interface.
+    /// </summary>
+    /// 
+    public class EdwChallengeClass2 : IEdwChallenge
+    {
+        /// <summary>
+        /// Report Instance information for
+        /// </summary>
+        public void Report()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// EdwChallengeClass3 SamPle Class Implementing the IEdwChallenge Interface.
+    /// </summary>
+    public class EdwChallengeClass3 : IEdwChallenge
+    {
+        public void Report()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
---
+++ ConsoleUI/EDW_Challenge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComponentLoader
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class EDW_Challenge:Attribute
+    {
+        string someText;
+        public EDW_Challenge() : this(string.Empty) { }
+        public EDW_Challenge(string txt) { someText = txt; }
+    }
+}
